15457c7e256c15c300b7ca29ba76a179b3ca31a2
Andrea Hasani <pasqyra3d@gmail.com>
2019-04-06 18:31:13 +0200

    Fixing performance issues on Velocity

---
+++ functions.py
@@ -0,0 +1,33 @@
+import requests
+import smtplib
+
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+
+def disposableEmail(email):
+    if not email:
+        return 0
+
+    r = requests.get(
+        "https://raw.githubusercontent.com/martenson/disposable-email-domains/master/disposable_email_blocklist.conf")
+    print(type(r.text))
+    urls = r.text.strip().split("\n")
+    urlTest = email.split("@")
+    if urlTest[-1] in urls:
+        return 1
+    else:
+        return 0
+
+
+def sendMessage(fromEmail, subject, message):
+    msg = MIMEText(message)
+    msg['From'] = fromEmail
+    msg['To'] = "email@andreahasani.com"
+    msg['Subject'] = subject
+    msg['Date'] = formatdate()
+    msg['X-Domain'] = "andreahasani.com"
+    msg['X-System'] = "python"
+
+    server = smtplib.SMTP('172.17.0.1')
+    server.sendmail(fromEmail, 'email@andreahasani.com', msg.as_string())
--- views.py
+++ views.py
@@ -1,9 +1,133 @@
-from flask import Flask, render_template, request, session, jsonify, abort
-from models import *
+import mistune
+import textwrap
+import requests
+
+# Import exceptions
+from lxml.etree import ParserError
+
+from flask import Flask, render_template, request, session, jsonify, abort, stream_with_context, Response
+from flask_compress import Compress
 from app import application
-from functions import *
+from functions import disposableEmail, sendMessage
+from lxml import html
+from os import listdir
+from os.path import isfile, join, getmtime
+
+renderer = mistune.Renderer(hard_wrap=True)
+markdown2html = mistune.Markdown(renderer=renderer)
 
 
-@application.route("/", methods=["GET"])
+# Compress
+COMPRESS_MIMETYPES = ['text/html', 'text/css', 'application/json']
+COMPRESS_LEVEL = 6
+COMPRESS_MIN_SIZE = 500
+Compress(application)
+
+
+@application.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    path = "./templates/blog/posts/"
+    posts = [f for f in listdir(path) if isfile(join(path, f))]
+    posts.sort(key=lambda x: getmtime(
+        join("./templates/blog/posts/", x)), reverse=True)
+    post_excerpt = []
+    for file in posts:
+        with open(join(path, file), "r") as post:
+            data = post.read()
+            body = markdown2html(data)
+            try:
+                doc = html.fromstring(body)
+            except ParserError as e:
+                abort(500)
+
+            excerpt = textwrap.shorten(doc.xpath("//p[1]/text()")[0], 200)
+            post_excerpt.append([file.replace("-", " ")[:-3].capitalize(),
+                                 excerpt])
+
+    if post_excerpt:
+        return render_template("index.html", publicationData=post_excerpt)
+    else:
+        return render_template("index.html")
+
+
+@application.route("/<title>", methods=["GET"], subdomain="blog")
+def blog_index(title):
+    get_post = title.replace("-", " ")
+    try:
+        with open("./templates/blog/posts/{}.md".format(get_post.replace(" ", "-")), 'r') as post:
+            data = post.read()
+        body = markdown2html(data)
+
+        return render_template("blog/post.html", Title=get_post.title(), post=body)
+    except FileNotFoundError:
+        abort(404)
+
+    except:
+        raise
+
+
+@application.route("/sendmail", methods=['GET'])
+def sendmail():
+    submitted = request.cookies.get('submitted')
+    if submitted:
+        msg = "<p>For security reasons you can only send one email every 1 min.</p>"
+        code = 403
+        sub = submitted + 1
+    else:
+        name = request.args.get('name')
+        email = request.args.get('email')
+        subject = request.args.get('subject')
+        message = request.args.get('message')
+        spamPro = request.args.get('first-name')
+
+        if not email or not subject or not name:
+            abort(403)
+
+        dispoFound = disposableEmail(email)
+
+        if spamPro:
+            msg = "<p>Please don't spam this form</p>"
+            code = 403
+        if dispoFound:
+            msg = "<p>Disposable email used. Please use your real email.</p>"
+            code = 403
+
+        else:
+            try:
+                sendMessage(email, '[%s] %s' % (name, subject), message)
+                msg = "<p>Thank you for contacting me. I will get back to you in 24 hours.</p>"
+                code = 200
+            except:
+                raise
+                msg = "<p>There was a problem sending the email. Please try again later.</p>"
+                code = 500
+
+    resp = jsonify(message=msg, code=code)
+    if code == 200 and not submitted:
+        resp.set_cookie('submitted', 'True', max_age=60)
+
+    return resp
+
+
+# Proxy for frank client, remove when setup on his site
+method_requests_mapping = {
+    'GET': requests.get,
+    'HEAD': requests.head,
+    'POST': requests.post,
+    'PUT': requests.put,
+    'DELETE': requests.delete,
+    'PATCH': requests.patch,
+    'OPTIONS': requests.options,
+}
+
+
+@application.route('/proxy/<path:url>', methods=method_requests_mapping.keys())
+def proxy(url):
+    requests_function = method_requests_mapping[request.method]
+    r = requests_function(
+        "https://" + url, stream=True, params=request.args)
+    response = Response(stream_with_context(r.iter_content()),
+                        content_type=r.headers['content-type'],
+                        status=r.status_code)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
