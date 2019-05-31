3cf97e04ef41c75d424d5a413a4e3cf22ef8a5c8
Andrea Hasani <pasqyra3d@gmail.com>
2019-05-31 22:27:27 +0200

    Adding navbar

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 // Global variables because of, why not
 var user_ip, user_interaction, user_location;
 var user_referrer = document.referrer;
+var nav_clicked_id;
 user_interaction = {
     "arrow_click": false,
     "resume": false,
@@ -53,18 +54,6 @@ $(document).ready(function () {
   var main_height = $(".sector-wrapper")[0].clientHeight;
   document.getElementsByClassName("sector-wrapper")[0].style.height = main_height + "px";
 });
-// Check for mobile
-window.mobilecheck = function () {
-  var check = false;
-
-  (function (a) {
-    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
-  })(navigator.userAgent || navigator.vendor || window.opera);
-
-  return check;
-}; // Fix for viewport min
-
-
 var viewport = document.querySelector("meta[name=viewport]");
 
 if (viewport) {
@@ -90,7 +79,7 @@ if (viewport) {
   }
 }
 
-var scroll = new SmoothScroll('a[id="arrow"]', {
+var scroll = new SmoothScroll('a.nav_a', {
 	// Selectors
 	topOnEmptyHash: true, // Scroll to the top of the page for links with href="#"
 
@@ -128,6 +117,11 @@ function paneNavigation(nextPane, oldPane, from) {
     $(".back-btn")[0].classList.remove("animate");
   }
 }
+
+
+$(".nav_a").on("click", function (e) {
+    nav_clicked_id = $(this).attr("data-goto");
+});
 
 $(".more .icons div > a").on("click", function (e) {
     e.preventDefault();
@@ -232,7 +226,29 @@ $(document).ready(function (e) {
 
     return false;
   });
+  var type_inst = new TypeIt("#typing", {
+    loop: true,
+    strings: ["builds websites.", "builds bots.", "builds cms.", "fixes wordpress problems.", "writes api.", "writes scrapers.", "deploys docker containers.", "writes python scripts."],
+    lifeLike: true,
+    cursorChar: "",
+    breakLines: false,
+    deleteSpeed: 60,
+    nextStringDelay: [600, 400]
+  }).go();
 });
+
+function displayMenu() {
+
+    var navbar = document.getElementById("nav");
+    var icon = document.getElementById("navbar_icon");
+    if (navbar.className === "navbar") {
+        icon.className += " change";
+        navbar.className += " responsive";
+    } else {
+        icon.className = "icon";
+        navbar.className = "navbar";
+    }
+}
 
 function ajax_get(url, callback) {
   var xmlhttp = new XMLHttpRequest();
@@ -295,5 +311,21 @@ ipLookUp();
     document.addEventListener('scrollStart', function(e) {
 	user_interaction['arrow_click'] = true;
 	user_interaction['active_pane'] = "About";
+    }, false);
+
+
+    document.addEventListener('scrollStop', function(e) {
+	console.log(nav_clicked_id);
+	if (nav_clicked_id != null) {
+	    if (nav_clicked_id == "publications") {}
+	    user_interaction[nav_clicked_id] = true;
+	    user_interaction["active_pane"] = nav_clicked_id;
+
+	    var nextPane = $("#" + nav_clicked_id);
+	    var oldPane = $(".pane-active");
+	    oldPane.removeClass("pane-active");
+	    nextPane.addClass("pane-active");
+	    paneNavigation(nextPane, oldPane, 0);
+	}
     }, false);
 });
