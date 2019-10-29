ab71a162b810d7b43e60d5df6ca5793800400e1f
Andrea Hasani <pasqyra3d@gmail.com>
2019-10-30 00:32:26 +0100

    Putting school date on resume

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/database'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 secret_key = os.urandom(24)
-# DEBUG = True
+DEBUG = True
 SERVER = "andreahasani.com"
-# PORT = 8080
-SERVER_NAME = SERVER
+PORT = 8080
+# SERVER_NAME = SERVER
